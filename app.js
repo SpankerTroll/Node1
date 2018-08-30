841e1afa478cad74fe584664a4d96d590c432bf1
SpankerTroll <spankertroll77@gmail.com>
2018-08-30 16:32:39 +0200

    Module bug fixed

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
 
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
-
-// Register listener
-emitter.on('Logging', function(arg){
-    console.log('Logging Device', arg);
-});
-
-const logger = require('./logger');
-logger.log('message');
-
